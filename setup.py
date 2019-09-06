85f40562546e2b34fad68f35b2bab0c62d78bb0f
Igor Aleksanov <popzxc@yandex.ru>
2019-09-06 10:04:40 +0300

    Add setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+INSTALL_REQUIRES = ["pyside2"]
+
+PYTHON_REQUIRES = ">=3.5"
+
+setup(
+    name="nutrition",
+    version="0.1",
+    description="Healthy nutrition app",
+    # url="https://github.com/TODO/TODO",
+    packages=["nutrition"],
+    install_requires=INSTALL_REQUIRES,
+    python_requires=PYTHON_REQUIRES,
+)
