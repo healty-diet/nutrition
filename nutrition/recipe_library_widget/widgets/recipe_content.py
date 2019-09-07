85270d832e99b3ace9f797f65d1b4aeb2654d148
Igor Aleksanov <popzxc@yandex.ru>
2019-09-07 16:25:55 +0300

    Use correct line separator

--- nutrition/recipe_library_widget/widgets/recipe_content.py
+++ nutrition/recipe_library_widget/widgets/recipe_content.py
@@ -1,5 +1,7 @@
 """ Recipe content widget. """
 
+import os
+
 from PySide2.QtWidgets import QPlainTextEdit
 
 from nutrition.recipe import Recipe
@@ -19,33 +21,37 @@ class RecipeContentWidget(QPlainTextEdit):
         recipe_text = ""
 
         # Set name.
-        recipe_text += "Название: {}\n".format(recipe.name)
-        recipe_text += "\n"
+        recipe_text += "Название: {}".format(recipe.name)
+        recipe_text += os.linesep + os.linesep
 
         # Set serves amount
-        recipe_text += "Количество порций: {}\n".format(recipe.serves_amount)
-        recipe_text += "\n"
+        recipe_text += "Количество порций: {}".format(recipe.serves_amount)
+        recipe_text += os.linesep + os.linesep
 
         # Set ingredients
-        recipe_text += "Ингредиенты:\n"
+        recipe_text += "Ингредиенты:" + os.linesep
         for ingredient in recipe.ingredients:
             # TODO make ingredient a dict-derived class with handy getters.
             ingredient_name = list(ingredient.keys())[0]
             ingredient_measure_name = MeasureName(list(ingredient[ingredient_name].keys())[0])
             ingredient_amount = ingredient[ingredient_name][ingredient_measure_name]
-            recipe_text += " - {} ({} {})\n".format(ingredient_name, ingredient_amount, ingredient_measure_name)
-        recipe_text += "\n"
+            recipe_text += " - {} ({} {})".format(ingredient_name, ingredient_amount, ingredient_measure_name)
+            recipe_text += os.linesep
+        recipe_text += os.linesep
 
         # Set recipe steps:
-        recipe_text += "Приготовление:\n"
+        recipe_text += "Приготовление:" + os.linesep
         recipe_text += recipe.text
-        recipe_text += "\n"
-        recipe_text += "\n"
+        recipe_text += os.linesep
+        recipe_text += os.linesep
 
         # Set calories data
         energy_data = recipe.energy_value_per_serving
-        recipe_text += "Калорийность на порцию: {:.1f} ккал ({:.1f} б, {:.1f} ж, {:.1f} у)\n".format(
-            energy_data.calories, energy_data.protein, energy_data.fat, energy_data.carbohydrates
+        recipe_text += (
+            "Калорийность на порцию: {:.1f} ккал ({:.1f} б, {:.1f} ж, {:.1f} у)".format(
+                energy_data.calories, energy_data.protein, energy_data.fat, energy_data.carbohydrates
+            )
+            + os.linesep
         )
 
         self.setPlainText(recipe_text)
